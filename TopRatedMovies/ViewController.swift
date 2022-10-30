3a7b3132dec2cc5d93b14aaf1bae542b99e3dede
Andrey Isaev <andron.isaev@gmail.com>
2022-10-30 13:47:50 +0600

    Initial commit

---
+++ TopRatedMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopRatedMovies
+//
+//  Created by Андрей Исаев on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
