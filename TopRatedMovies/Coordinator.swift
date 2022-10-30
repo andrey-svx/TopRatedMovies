20a13c89e79bcf39c29df671ce004bbe3cf5bb88
Andrey Isaev <andron.isaev@gmail.com>
2022-10-30 14:06:10 +0600

    Added base class, and protocol

---
+++ TopRatedMovies/Coordinator.swift
@@ -0,0 +1,42 @@
+//
+//  Coordinator.swift
+//  TopRatedMovies
+//
+//  Created by Андрей Исаев on 30.10.2022.
+//
+
+import UIKit
+
+protocol Completable {
+    
+    associatedtype T
+    
+    var onComplete: ((T) -> Void)? { get set }
+}
+
+open class Coordinator: NSObject {
+        
+    let navigationController: UINavigationController
+    
+    var id: Int { hashValue }
+    
+    var childCoordinators: [Int: Coordinator] = [:]
+    
+    init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func retain(_ coordinator: Coordinator) {
+        childCoordinators[coordinator.id] = coordinator
+    }
+
+    func release(_ coordinator: Coordinator) {
+        childCoordinators[coordinator.id] = nil
+    }
+
+    func releaseAll() {
+        childCoordinators.removeAll()
+    }
+    
+    func start(_ releaser: @escaping () -> Void = { }) { }
+}
