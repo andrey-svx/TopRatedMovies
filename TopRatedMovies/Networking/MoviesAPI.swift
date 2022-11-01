2c70b568a55dbb078026ed1b9ea90d58251834ac
Andrey Isaev <andron.isaev@gmail.com>
2022-11-02 02:51:26 +0600

    Added MoviesAPI

--- TopRatedMovies/Networking/MoviesAPI.swift
+++ TopRatedMovies/Networking/MoviesAPI.swift
@@ -9,5 +9,37 @@ import Foundation
 import Moya
 
 enum MoviesAPI {
+    case topRated
+}
+
+extension MoviesAPI: TargetType {
     
+    var baseURL: URL {
+        URL(string: APIConfigProvider.shared.host)!
+    }
+    
+    var path: String {
+        switch self {
+        case .topRated:
+            return "3/movie/top_rated"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .topRated:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .topRated:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        nil
+    }
 }
