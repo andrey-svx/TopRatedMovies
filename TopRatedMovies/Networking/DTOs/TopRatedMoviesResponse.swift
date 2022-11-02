71611fb41fb59cce3b1a839a32081d72c9c8697f
Andrey Isaev <andron.isaev@gmail.com>
2022-11-02 23:49:55 +0600

    Added fields to movies response

--- TopRatedMovies/Networking/DTOs/TopRatedMoviesResponse.swift
+++ TopRatedMovies/Networking/DTOs/TopRatedMoviesResponse.swift
@@ -13,17 +13,21 @@ struct TopRatedMoviesResponse: Decodable {
     
     struct Result: Decodable {
         
+        let id: Int
         let popularity: Double
         let posterPath: String
         let releaseDate: String
         let title: String
+        let voteAverage: Double
         
         private enum CodingKeys: String, CodingKey {
             
+            case id
             case popularity
             case posterPath = "poster_path"
             case releaseDate = "release_date"
             case title
+            case voteAverage = "vote_average"
         }
     }
 }
